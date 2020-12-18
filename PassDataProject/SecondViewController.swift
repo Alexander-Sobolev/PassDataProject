1c0e9107da64c35e41445025edad2b028ae10316
Alexander Sobolev <sobolev.ios.developer@gmail.com>
2020-12-18 22:04:25 +0300

    Don't used constraints

---
+++ PassDataProject/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  PassDataProject
+//
+//  Created by Alexander Sobolev on 18.12.2020.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func goBackTapped(_ sender: Any) {
+    }
+}
--- PassDataProject/ViewController.swift
+++ PassDataProject/ViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var loginTF: UITextField!
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
     }
-
-
 }
 
